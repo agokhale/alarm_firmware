42b8532c2d7b072a80a21b9d747fdb055432b499
xpi <you@example.com>
2020-06-05 13:21:51 -0400

    fix moving avg.

--- firmware/Inc/EMA_crush.h
+++ firmware/Inc/EMA_crush.h
@@ -1,9 +1,20 @@
 #ifndef EMA_cushhh
 #define EMA_crushh
 
-// provide a exponential moving average macro that should be unitless
+/*  provide a exponential moving average macro that should be unitless
+ simple moving average
+vars: 
+	in: most current value
+	ema: last value, also overwritten in this operation
+	rate: smoothing rate,  divisor for the delta to be applied to the old value; extra credit for useing powers of 2, which saves some cpu time.
 
-//simple moving average
-#define EMA_crush(in,ema,rate)  (ema = (in+((rate-1)*ema))/rate)
+usage:  EMA_crush(newval, stored_ema, 32);
+*/
+
+#define EMA_crush(in,ema,rate)  (  \
+	ema = ( \
+		 ema + (in - ema)/rate  \
+		)  \
+	)
 
 #endif
